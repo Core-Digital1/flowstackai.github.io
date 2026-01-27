5555157fe30be218b5150e08cccbefe1819f26d4
Amadej Fink <amadej.fink@icloud.com>
2026-01-27 14:57:46 +0100

    feat: Create initial React application structure with React Router for page navigation.

--- ClipForgeAI/src/App.jsx
+++ ClipForgeAI/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import Layout from './Layout';
 import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
-        <Layout currentPageName="Home">
-            <LandingPage />
-        </Layout>
+        <HelmetProvider>
+            <BrowserRouter basename="/ClipForgeAI">
+                <Routes>
+                    <Route path="/" element={
+                        <Layout currentPageName="Home">
+                            <LandingPage />
+                        </Layout>
+                    } />
+                    <Route path="/dashboard" element={
+                        <Layout currentPageName="Dashboard">
+                            <Dashboard />
+                        </Layout>
+                    } />
+                </Routes>
+            </BrowserRouter>
+        </HelmetProvider>
     );
 }
 
